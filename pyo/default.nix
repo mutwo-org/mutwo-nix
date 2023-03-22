9e435c35cc7e58411050f2f8e1442a1a05804b81
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-22 19:48:50 +0100

    Add pyo
    It's good to also add walkman stack here, because often enough we need
    both together.

---
+++ pyo/default.nix
@@ -0,0 +1,33 @@
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nixpkgs {}}:
+with pkgs;
+with pkgs.python310Packages;
+
+buildPythonPackage rec {
+  name = "pyo";
+  src = fetchFromGitHub {
+    owner = "belangeo";
+    repo = "pyo";
+    rev = "360c429138e170e291a89da605a8dc20f837466f";
+    sha256 = "sha256-CRHl+lAdiDXrXVcr9W14XE9lTJ6T7MfE8WeTMm6FpzU=";
+  };
+  doCheck = false;
+  setupPyGlobalFlags = [
+      "--use-double"
+      "--use-jack"
+  ];
+  propagatedBuildInputs = with pkgs; [ 
+    # C library dependencies for pyo
+    portaudio
+    libsndfile
+    portmidi
+    liblo
+    alsa-lib
+    alsa-utils
+    libjack2
+    flac
+    libogg
+    libvorbis
+    # Other pyo dependencies
+    wxPython_4_1
+  ];
+}
