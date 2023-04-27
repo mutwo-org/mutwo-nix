44e6bc467017c553970ed757dbc1a1eecc938096
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-04-27 08:48:53 +0200

    Add mutwo.csound

---
+++ mutwo.csound/default.nix
@@ -0,0 +1,33 @@
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nixpkgs {}}:
+with pkgs;
+with pkgs.python310Packages;
+
+let
+
+  mutwo-core = import ../mutwo.core/default.nix {};
+  origin = sources.mutwo-csound;
+
+in
+
+  buildPythonPackage rec {
+    name = origin.repo;
+    src = fetchFromGitHub {
+      owner = origin.owner;
+      repo = origin.repo;
+      rev = origin.rev;
+      sha256 = origin.sha256;
+    };
+    nativeCheckInputs = [ pytest ];
+    checkInputs = [ pytest ];
+    propagatedBuildInputs = [ 
+      mutwo-core
+      natsort
+    ];
+    checkPhase = ''
+      runHook preCheck
+      pytest
+      pytest --doctest-modules mutwo
+      runHook postCheck
+    '';
+    doCheck = true;
+  }
