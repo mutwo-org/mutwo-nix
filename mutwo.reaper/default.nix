659738ee70b2fe07cfc55c9058b57ee726be3389
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-11-13 22:13:43 +0100

    Add mutwo.reaper

---
+++ mutwo.reaper/default.nix
@@ -0,0 +1,33 @@
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nixpkgs {}, pythonPackages ? pkgs.python310Packages}:
+with pkgs;
+with pythonPackages;
+
+let
+
+  args = {sources=sources; pkgs=pkgs; pythonPackages=pythonPackages;};
+  mutwo-core = import ../mutwo.core/default.nix args;
+  origin = sources.mutwo-reaper;
+
+in
+
+  buildPythonPackage rec {
+    name = origin.repo;
+    src = fetchFromGitHub {
+      owner = origin.owner;
+      repo = origin.repo;
+      rev = origin.rev;
+      sha256 = origin.sha256;
+    };
+    nativeCheckInputs = [ pytest ];
+    checkInputs = [ pytest ];
+    propagatedBuildInputs = [ 
+      mutwo-core
+    ];
+    checkPhase = ''
+      runHook preCheck
+      pytest
+      pytest --doctest-modules mutwo
+      runHook postCheck
+    '';
+    doCheck = true;
+  }
