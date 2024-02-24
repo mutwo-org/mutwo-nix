47bad2fd4190e76608053ae0f0ac0ab9562acfeb
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2024-02-24 22:45:36 +0100

    Add mutwo.mmml

---
+++ mutwo.mmml/default.nix
@@ -0,0 +1,34 @@
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nixpkgs {}, pythonPackages ? pkgs.python310Packages}:
+with pkgs;
+with pythonPackages;
+
+let
+
+  args = {sources=sources; pkgs=pkgs; pythonPackages=pythonPackages;};
+  mutwo-music = import ../mutwo.music/default.nix args;
+  origin = sources.mutwo-mmml;
+
+in
+
+  buildPythonPackage rec {
+    name = "mutwo.mmml";
+    src = fetchFromGitHub {
+      owner = origin.owner;
+      repo = origin.repo;
+      rev = origin.rev;
+      sha256 = origin.sha256;
+    };
+    nativeCheckInputs = [ pytest ];
+    checkInputs = [ pytest ];
+    propagatedBuildInputs = [ 
+      mutwo-music
+      chevron
+    ];
+    checkPhase = ''
+      runHook preCheck
+      pytest
+      pytest --doctest-modules mutwo
+      runHook postCheck
+    '';
+    doCheck = true;
+  }
